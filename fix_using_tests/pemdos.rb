e02c993a48699f710f3626632c62598505dcf21b
Joumana Laude <joumanalaude@gmail.com>
2020-08-04 01:23:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,9 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-  snake_it_up = "surprise!"
+  snake_it_up == "surprise!"
+  puts snake_it_up
+  binding.pry
   if string[0] == "s"
     10.to_s * "s" + string
   else
