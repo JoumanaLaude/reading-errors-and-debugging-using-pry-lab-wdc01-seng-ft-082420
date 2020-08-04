ccef7fa3d3cb2d8113093e03fa27843d67b88ca2
Joumana Laude <joumanalaude@gmail.com>
2020-08-04 01:40:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,7 @@
 require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-      snake_it_up = 10 * "s" + string
-                                                                    # 10 * "s" + string
+    10 * "s" + string
   else
     string
   end
