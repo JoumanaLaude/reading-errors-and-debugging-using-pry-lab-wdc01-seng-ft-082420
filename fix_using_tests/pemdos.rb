2cc6a522def527962d5236fe537cbf1c7e92c2d6
Joumana Laude <joumanalaude@gmail.com>
2020-08-04 01:25:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,6 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-  snake_it_up = "surprise!"
-
-  puts snake_it_up
-
   if string[0] == "s"
     10.to_s * "s" + string
     # 10 * "s" + string
