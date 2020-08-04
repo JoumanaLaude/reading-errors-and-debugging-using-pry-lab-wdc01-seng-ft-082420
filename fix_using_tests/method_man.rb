2419f177433e30ef29bb3005a46c06a042435b04
Joumana Laude <joumanalaude@gmail.com>
2020-08-04 01:08:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,13 @@
 # don't forget to add: require 'pry'
-
-def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+require 'pry'
+def start_game(player1,player2)
+  player1 == "Rocky"
+  player2 == "Bullwinkle"
+  start_game = "Hello #{player1} & #{player2}"
 end
-
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+  
+def play_game(player1,player2)
+  player1 == "JayZ"
+  player2 == "Beyonce"
+  play_game = "#{player1} is better than #{player2}"
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,13 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
+  snake_it_up = "surprise!"
   if string[0] == "s"
-    10 * "s" + string
+    10.to_s * "s" + string
   else
     string
   end
+
+puts snake_it_up
+
 end
